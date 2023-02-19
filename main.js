2f33741c497e4b2765c85af2f84fff8e5831630a
dayana-pv <dayana.priscilla.17@gmail.com>
2023-02-18 21:41:35 -0500

    Se añadió la búsqueda de libros

--- main.js
+++ main.js
@@ -1,87 +1,100 @@
-const libros = [
+let libros = [
   {
     id: 1,
+    imagen: "imagenes/naturalezaFria.jpg",
     nombre: "Naturaleza Fria",
     autor: "Shepherd, Megan",
     precio: 19,
   },
   {
     id: 2,
+    imagen: "imagenes/readyPlayerOne.jpg",
     nombre: "Ready Player One",
     autor: "Cline, Ernest",
     precio: 99,
   },
   {
     id: 3,
+    imagen: "imagenes/llanuraTenebrosa.jpg",
     nombre: "Una Llanura Tenebrosa",
     autor: "Reeve, Philip",
     precio: 20,
   },
   {
     id: 4,
+    imagen: "imagenes/despuesDeNieve.jpg",
     nombre: "Después de la Nieve",
     autor: "Crockett, S. D.",
     precio: 30,
   },
+  {
+    id: 5,
+    imagen: "imagenes/readyPlayerOne2.jpg",
+    nombre: "Ready Player One",
+    autor: "Cline, Ernest",
+    precio: 99,
+  },
+  {
+    id: 6,
+    imagen: "imagenes/elExorcista.jpg",
+    nombre: "El Exorcista",
+    autor: "Blatty, William Peter",
+    precio: 59,
+  },
+  {
+    id: 7,
+    imagen: "imagenes/laCiudadYLosPerros.jpg",
+    nombre: "La Ciudad y los Perros",
+    autor: "Vargas Llosa, Mario",
+    precio: 49,
+  },
+  {
+    id: 8,
+    imagen: "imagenes/mickey7.jpg",
+    nombre: "Mickey7",
+    autor: "Ashton, Edward",
+    precio: 50,
+  },
 ];
 
-function registrarUsuario() {
-  let usuario = prompt("Ingrese el nombre de su usuario");
-  let correo = prompt("Ingrese su correo");
-  if (usuario !== "" && correo !== "") {
-    alert(`Hola ${usuario}, tu correo "${correo}" ha sido registrado!`);
-    mostrarLibros();
-    comprarLibro();
-  } else {
-    alert(
-      `Debe registrar su usuario y correo para poder ingresar a la libreria!`
-    );
-  }
+function mostrarLibros(libros) {
+  let container = document.getElementById("libros");
+  container.innerHTML = ``;
+
+  libros.forEach((item) => {
+    let libro = document.createElement("div");
+    libro.innerHTML = `
+    <div class="max-w-sm rounded overflow-hidden shadow-lg ">
+        <img class="object-contain h-30 w-full" src=${item.imagen} >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2" >${item.nombre}</div>
+          <p class="text-gray-700 text-base">${item.autor}</p>
+          <b>$${item.precio}</b>
+        </div>
+    </div>
+    `;
+    container.append(libro);
+  });
 }
 
-function mostrarLibros() {
-  let mensaje = `  Bienvenida a la Libreria!!
-  Los Libros que tenemos disponibles son: 
-  `;
-  for (let index = 0; index < libros.length; index++) {
-    mensaje += `
-        Nombre: ${libros[index].nombre}
-        Autor: ${libros[index].autor}
-        Precio: S/.${libros[index].precio}
-     `;
-  }
-  alert(mensaje);
-}
+function buscarLibro() {
+  let formulario = document.getElementById("formulario");
 
-function comprarLibro() {
-  let libro = prompt("Escriba el nombre del libro que desea comprar:");
-  if (libro !== "") {
-    for (let index = 0; index < libros.length; index++) {
-      if (libro === libros[index].nombre) {
-        resultado = true;
-        break;
-      } else {
-        resultado = false;
-      }
-    }
-  } else {
-    alert("Debe ingresar el nombre del libro");
-  }
+  formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputs = e.target.children;
 
-  if (resultado == true) {
-    let libroPedido = prompt(
-      "El libro se encuentra disponible, ¿Le gustaria comprarlo?"
+    let libroObtenido = libros.filter(
+      (libro) => libro.nombre.toLowerCase() === inputs[0].value.toLowerCase()
     );
-    if (libroPedido.toLowerCase() === "si") {
-      alert(
-        "Se enviara los datos para el proceso de pago a su correo. Gracias por su compra!"
-      );
+
+    if (libroObtenido.length > 0) {
+      mostrarLibros(libroObtenido);
     } else {
-      alert("Puede seguir buscando los libros que le gusten");
+      alert(`El libro "${inputs[0].value}" no se encuentra en el sistema`);
     }
-  } else {
-    alert("El libro no se encuentra disponible por el momento");
-  }
+  });
 }
 
-registrarUsuario();
+mostrarLibros(libros);
+buscarLibro();
